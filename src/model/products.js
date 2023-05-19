06aa5838067bfd82644590f5a0a0c5c826e07453
cameochoquer <cjchoquer@gmail.com>
2023-05-19 20:38:08 +0100

    completed challenege 1 + 2 passing tests

--- src/model/products.js
+++ src/model/products.js
@@ -1,7 +1,36 @@
 const db = require("../database/db.js");
 
-const listProducts = () => {
+//challenge 1
+const select_products = db.prepare(/*sql*/
+    `SELECT 
+    id,
+    name,
+    quantity_per_unit,
+    unit_price,
+    units_in_stock,
+    units_on_order
+    FROM products
+    `
+);
 
+const listProducts = () => {
+return select_products.all();
+};
+let allProducts = listProducts();
+console.log(allProducts.name)
+
+//challenge 2
+
+const search_products = db.prepare(/*sql*/ `
+  SELECT
+    id,
+    name
+  FROM products
+  WHERE name LIKE ?
+`);
+const searchProducts = (searchString) =>{
+return search_products.all(`%${searchString}%`)
 };
 
-module.exports = { listProducts }
+
+module.exports = { listProducts, searchProducts }
